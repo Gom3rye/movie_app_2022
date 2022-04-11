c1edec7878b40308cf41f620f3c4bf56824eeca0
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2022-04-11 21:33:59 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovieData = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({ isLoading: false, movies });
+  };
+  componentDidMount() {
+    // setTimeout(() => {
+    //   this.setState({ isLoading: false });
+    // }, 6000);
+    this.getMovieData();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section>
+        {isLoading ? (
+          <div>
+            <span>Loading ...</span>
+          </div>
+        ) : (
+          <div>
+            {movies.map((data) => {
+              return (
+                <Movie
+                  key={data.id}
+                  id={data.id}
+                  title={data.title}
+                  year={data.year}
+                  summary={data.summary}
+                  genres={data.genres}
+                  cover_img={data.medium_cover_image}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
